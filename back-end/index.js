9e900138d73f518183842735e4bc2e4f4a49ddf7
KoEunsaem <koensm12@gmail.com>
2023-11-25 06:03:15 +0900

    connect

--- back-end/index.js
+++ back-end/index.js
@@ -9,14 +9,18 @@ app.listen(8080,function(){
 	console.log('listening on 8080')
 });
 
+// main 페이지
+app.post('/main', function(req, res){
+    res.send();
+});
+
+// 회원가입 page
+app.get('/sign', function(req, res){
+});
+
+// 로그인 page
+app.get('/login', function(req, res){
+});
+
 const userTime = '2시간';
 fetchData(userTime); 
-
-
-
-
-app.get('/', function(req, res){
-    res.send("하이하이");
-    // const filePath = path.join(__dirname, '../front-end/mainchoose.html');
-    // res.sendFile(filePath);
-});
