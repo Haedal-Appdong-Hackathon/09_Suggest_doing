ed0e8e406bcbf95269b4215581962f79a33466e7
KoEunsaem <koensm12@gmail.com>
2023-11-25 05:24:51 +0900

    update API

--- back-end/apiCaller.js
+++ back-end/apiCaller.js
@@ -1,12 +1,26 @@
-const openAI = require('openai');
+const env = require('dotenv');
+const OpenAI= require('openai');
 
-function callOpenAI(){
-    const openai = new openAI({apiKey : "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"});
+env.config();
 
-    return openai.chat.completions.create({
-        messages: [{ role: "user", content: "Say this is a test" }],
-        model: "gpt-3.5-turbo",
-    });
-}
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
-module.exports = {callOpenAI};
+async function fetchData(time) {
+    try {
+        const wpqkf = await openai.completions.create({
+            model: "text-davinci-003",
+            prompt : `${time} 동안 할 일 추천해줘`,
+            max_tokens: 42,
+            temperature: 0,
+        });
+        
+        console.log(wpqkf.choices[0].text);
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  module.exports = {fetchData};
--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const { callOpenAI } = require('./apiCaller');
+const { fetchData } = require('./apiCaller');
 const { error } = require('console');
 
 const app = express();
@@ -9,25 +9,14 @@ app.listen(8080,function(){
 	console.log('listening on 8080')
 });
 
+const userTime = '2시간';
+fetchData(userTime); 
+
+
+
+
 app.get('/', function(req, res){
     res.send("하이하이");
     // const filePath = path.join(__dirname, '../front-end/mainchoose.html');
     // res.sendFile(filePath);
 });
-
-function recommandTask(time)
-{
-
-    return callOpenAI()
-        .then((response) => {
-            const recommandation = response.data.choices[0].text;
-            console.log(`남는 ${time} 동안`, recommandation, "을 추천드려요");
-        })
-
-        .catch((error) => {
-            console.log('error: ', error);
-        });
-}
-
-const userTime = '2시간';
-recommandTask(userTime); 
